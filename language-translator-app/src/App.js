5b66119d5bfe27086aa30c4e4fe5d1743198a003
Prabodhi <prabodhi38@gmail.com>
2023-04-18 11:34:50 +0200

    adding css

--- language-translator-app/src/App.js
+++ language-translator-app/src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
-// import "./styles.css";
+import "./styles.css";
 import Field from "./components/field";
 import Languages from "./components/languages";
 import Translate from "./components/translate";
 
 function App() {
   return (
-    <div>
+    <div className="main-container">
       <Field />
       <Languages />
       <hr />
